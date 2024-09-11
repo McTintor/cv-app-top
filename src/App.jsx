9f27e368e7ebf0411f99d49acc871a833d7da0af
McTintor <igortintor99@gmail.com>
2024-09-11 13:21:32 +0200

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='input-div'>
+
+      </div>
+      <div className='display-div'>
+
+      </div>
+    </>
+  )
+}
+
+export default App
